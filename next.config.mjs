6450204fece4b9ee1ea2c48b399bc7958d29bd73
conte0745 <ryoya15662@gmail.com>
2024-02-28 01:02:18 +0900

    change setValue

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
-export async function headers() {
-	return [
-		{
-			source: "/api/todo/:path*",
-			headers: [
-				{
-					key: "Access-Control-Allow-Origin",
-					value: "*", // 許可するオリジンを適切に指定する
-				},
-				{
-					key: "Access-Control-Allow-Methods",
-					value: "GET POST PUT DELETE OPTIONS",
-				},
-				{
-					key: "Access-Control-Allow-Headers",
-					value: "Content-Type, Authorization",
-				},
-			],
-		},
-	];
-}
+// next.config.js
+export const experimental = {
+	modern: true,
+};
