15d47b4bb9223567ff03b12abe687cf701d8fe07
conte0745 <ryoya15662@gmail.com>
2024-04-14 18:25:20 +0900

    テスト修正

--- __tests__/ui/topPageUI.test.ts
+++ __tests__/ui/topPageUI.test.ts
@@ -4,28 +4,21 @@ describe("トップページのテスト", () => {
 	let baseUrl: string;
 	let driver: ThenableWebDriver;
 
-	// テスト前のセットアップ
 	beforeAll(async () => {
-		// WebDriverのセットアップ
 		driver = new Builder().forBrowser("chrome").build();
 		baseUrl = "http://localhost:3000";
 	});
 
-	// テスト後のクリーンアップ
 	afterAll(async () => {
 		await driver.quit();
 	});
 
 	it("titleが予期されているものと一致するかどうかのテスト", async () => {
 		await driver.get(baseUrl);
-
-		// タイトルが反映されるのを待つ
 		await driver.wait(until.titleContains("RetainTutor"), 5000);
 
-		// 実際のタイトルを取得
 		const title = await driver.getTitle();
 
-		// 期待されるタイトルと実際のタイトルを比較
 		expect(title).toBe("RetainTutor");
 	});
 
@@ -41,22 +34,17 @@ describe("トップページのテスト", () => {
 		const linkItems = await driver.findElements(
 			By.className("top-page-link-list-item")
 		);
-
-		// リンクを順番に検証する
 		for (let idx = 0; idx < linkItems.length; idx++) {
 			const linkItem = linkItems[idx];
 			const linkInfoItem = linkInfo[idx];
 
 			await linkItem.click();
 
-			// URLが正しいかどうかを検証
 			let currentUrl = await driver.getCurrentUrl();
 			expect(currentUrl).toBe(baseUrl + linkInfoItem.url);
 
-			// 戻るボタンをクリックして元のページに戻る
 			await driver.navigate().back();
 
-			// 再度URLが正しいかどうかを検証
 			currentUrl = await driver.getCurrentUrl();
 			expect(currentUrl).toBe(baseUrl);
 		}
@@ -67,31 +55,25 @@ describe("トップページのテスト", () => {
 
 		const githubUrl = "https://github.com/conte0745/retain";
 
-		// powered by テキストが含まれることを確認
 		const linkTextItem = await driver.wait(
 			until.elementLocated(By.id("powered-by-author"))
 		);
 		const linkText = await linkTextItem.getText();
 		expect(linkText).toContain("powered by");
 
-		// リンクを検証する
 		const linkItem = await driver.wait(
 			until.elementLocated(By.id("powered-by-author-link"))
 		);
 		await linkItem.click();
 
-		// 新しいタブが開かれるのを待つ
 		await driver.wait(async () => {
 			const handles = await driver.getAllWindowHandles();
 			return handles.length === 2;
 		}, 5000);
-
-		// 新しいタブに切り替える
 		const handles = await driver.getAllWindowHandles();
 		const newTabHandle = handles[1];
 		await driver.switchTo().window(newTabHandle);
 
-		// URLが正しいかどうかを検証
 		const currentUrl = await driver.getCurrentUrl();
 		expect(currentUrl).toBe(githubUrl);
 	});
